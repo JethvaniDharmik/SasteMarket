74936b8af42331c09e339527b3d297f12840ad38
JethvaniDharmik <dharmikjethwani777@gmail.com>
2025-09-19 20:03:55 +0530

    Fix import path case sensitivity issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,48 @@
-// this file used for link to all pages or componets 
+// App.jsx
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
-import Services from './pages/services'
+
+// ✅ Pages
+import Home from './pages/Home'
+import Services from './pages/Services'
 import Login from './pages/Login'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
+
+// ✅ Components
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { ToastContainer, toast } from 'react-toastify';
 
-
+// ✅ Toastify
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // ⚡ make sure you import css also
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-     {/* thay both of tham are components */}
+      {/* Toast container for notifications */}
       <ToastContainer />
+      
+      {/* Common components */}
       <Navbar />
-      {/* thay all routes link multipal link to each to other  */}
-      {/* and router use for without load any page move to next page  */}
+
+      {/* Routes */}
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/services/:speciality" element={<Services />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/my-profile" element={<MyProfile />} />
-      <Route path="/my-appointments" element={<MyAppointments />} />
-      <Route path="/appointment/:serId" element={<Appointment />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/services/:speciality" element={<Services />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointment/:serId" element={<Appointment />} />
       </Routes>
-    {/* also one components */}
+
+      {/* Footer */}
       <Footer />
     </div>
   )
